49d7ecc4174256c0bdbb8274e0557217f236af6c
JW <JW00000@gmail.com>
2008-03-07 21:16:17 +0100

    Add an /index.json action, to get all entries in JSON format.

--- init.rb
+++ init.rb
@@ -1,10 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 require 'sequel'
+require 'json'
 
 # Connect to the database
 DB = Sequel.sqlite('blog.db')
 
 get '/' do
   haml :home
+end
+
+# Get all the entries in JSON format
+get '/index.json' do
+  entries = DB[:entries].all
+  entries.to_json()
 end
