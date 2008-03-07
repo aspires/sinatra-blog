b0cde5f0b58dc325c0f1394dce1abefd2b3cd5c6
JW <JW00000@gmail.com>
2008-03-07 21:22:00 +0100

    Start an /entry/:slug action.

--- init.rb
+++ init.rb
@@ -17,6 +17,18 @@ get '/index.json' do
   entries.to_json()
 end
 
+# View an entry
+get '/entry/:slug' do
+  entry = DB[:entries][:slug => params[:slug]]
+  if entry
+    haml :entry
+  else
+    status 404
+    #haml :notfound -- TODO: Create a not found page.
+    body "Entry not found."
+  end
+end
+
 # Get an entry in JSON format
 get '/entry/:slug.json' do
   entry = DB[:entries][:slug => params[:slug]]
