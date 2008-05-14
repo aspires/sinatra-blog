f6c94ffce8c36b904708e58f85b30b1d0c6397bc
JW <JW00000@gmail.com>
2008-05-14 18:40:29 +0200

    Wrap the code to load the configuration file and connect to the database in a "configure do ... end" block.

--- init.rb
+++ init.rb
@@ -13,20 +13,22 @@ require 'sequel'
 require 'json'
 require 'yaml'
 
-# Load the configuration file.
-if !File.exist?('config.yml')
-  puts "There's no configuration file at config.yml!"
-  exit!
-end
-CONFIG = YAML.load_file('config.yml')
+configure do
+  # Load the configuration file.
+  if !File.exist?('config.yml')
+    puts "There's no configuration file at config.yml!"
+    exit!
+  end
+  CONFIG = YAML.load_file('config.yml')
 
-# Connect to the database
-if !File.exist?('blog.db')
-  puts "There's no database file at blog.db! Create an empty file called " +
-       "blog.db and run 'ruby create_db.rb' to create the database."
-  exit!
+  # Connect to the database
+  if !File.exist?('blog.db')
+    puts "There's no database file at blog.db! Create an empty file called " +
+        "blog.db and run 'ruby create_db.rb' to create the database."
+    exit!
+  end
+  DB = Sequel.sqlite('blog.db')
 end
-DB = Sequel.sqlite('blog.db')
 
 # Create the models.
 Dir["app/models/*.rb"].each { |x| require x }
